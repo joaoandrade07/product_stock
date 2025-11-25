21e776c62fdeb92c592f8ce078b12a6cede3e8c2
João Andrade <joaodeandrade07@gmail.com>
2025-11-25 14:35:21 -0300

    :sparkles: feat: Added token generation with expiration time

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -4,7 +4,14 @@ import { IJwtPayload } from "../interfaces/Jwt";
 export const generateToken = (user: IJwtPayload) => {
     return jwt.sign(
         {id: user.id, role: user.role},
+        process.env.JWT_SECRET as string
+    );
+}
+
+export const generateExpiringToken = (user:IJwtPayload, hours:number = 3) => {
+    return jwt.sign(
+        {id: user.id, role: user.role},
         process.env.JWT_SECRET as string,
-        //{expiresIn: "1h"}
+        {expiresIn: `${hours}h`}
     );
 }
